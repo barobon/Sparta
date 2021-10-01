32b44e57b4ecc51d0ce78db37281a87aed019a36
barobon <46195201+barobon@users.noreply.github.com>
2021-10-01 16:54:32 +0900

    add gameover function

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,5 +11,14 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void GameOver()
+    {
+        Invoke("GameOver_", 2);
+    }
+    void GameOver_()
+    {
+        SceneManager.LoadScene(0);
     }
 }
